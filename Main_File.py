3addc4d6994e20ee802ad0eecec8903746d4b07f
Yohaan-TheTechGod <yohaandosaj123@gmail.com>
2025-04-11 11:28:50 +1000

    problem with background image, fixed it

--- Main_File.py
+++ Main_File.py
@@ -15,7 +15,7 @@ def open_main_page(root):
     tk.Button(root, text="Quiz", command=lambda: quiz_page.open_quiz_page(root, open_main_page), width=15).pack(pady=10)
     tk.Button(root, text="About", command=lambda: about_page.open_about_page(root, open_main_page), width=15).pack(pady=10)
 
-image_path=PhotoImage(file=r"/Users/yohaan_thetechgod/Desktop/Year 11 Stuff/Projects/Software Engineering/Software Assesment 1/Periodic table background.jpg")
+image_path=PhotoImage(file="/Users/yohaan_thetechgod/Desktop/Year 11 Stuff/Projects/Software Engineering/Software Assesment 1/Periodic table background.jpg")
 bg_image= tk.label(root,image= image_path)
 bg_image.place(relheight=1, relwidth=1)
 root = tk.Tk()
