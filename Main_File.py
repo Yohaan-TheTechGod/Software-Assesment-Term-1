729b122c34bc5c73f14dda8ed99894a8b0858de9
Yohaan-TheTechGod <yohaandosaj123@gmail.com>
2025-04-11 11:23:46 +1000

    added a background image to the Main_File

--- Main_File.py
+++ Main_File.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import PhotoImage
+root=tk
 import learn_page
 import quiz_page
 import about_page
@@ -13,7 +15,9 @@ def open_main_page(root):
     tk.Button(root, text="Quiz", command=lambda: quiz_page.open_quiz_page(root, open_main_page), width=15).pack(pady=10)
     tk.Button(root, text="About", command=lambda: about_page.open_about_page(root, open_main_page), width=15).pack(pady=10)
 
-
+image_path=PhotoImage(file=r"/Users/yohaan_thetechgod/Desktop/Year 11 Stuff/Projects/Software Engineering/Software Assesment 1/Periodic table background.jpg")
+bg_image= tk.label(root,image= image_path)
+bg_image.place(relheight=1, relwidth=1)
 root = tk.Tk()
 root.title("Simple Quiz App")
 root.geometry("400x300")
