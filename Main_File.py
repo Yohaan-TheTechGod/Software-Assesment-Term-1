9ee08695e8bc641873fb848d22af408eb41687b9
Yohaan-TheTechGod <yohaandosaj123@gmail.com>
2025-04-11 12:12:48 +1000

    added an exit button and made background

--- Main_File.py
+++ Main_File.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import PhotoImage
-root=tk
 import learn_page
 import quiz_page
 import about_page
@@ -9,17 +8,20 @@ def open_main_page(root):
   
     for widget in root.winfo_children():
         widget.destroy()
+    
+    image_path=PhotoImage(file="/Users/yohaan_thetechgod/Desktop/Year 11 Stuff/Projects/Software Engineering/Software Assesment 1/Periodic table background.png")
+    bg_image= tk.Label(root,image= image_path)
+    bg_image.image= image_path
+    bg_image.place(relheight=1, relwidth=1)
 
 
     tk.Button(root, text="Learn", command=lambda: learn_page.open_learn_page(root, open_main_page), width=15).pack(pady=10)
     tk.Button(root, text="Quiz", command=lambda: quiz_page.open_quiz_page(root, open_main_page), width=15).pack(pady=10)
     tk.Button(root, text="About", command=lambda: about_page.open_about_page(root, open_main_page), width=15).pack(pady=10)
+    tk.Button(root, text="Exit", command=root.quit, width=15).pack(pady=10)
 
-image_path=PhotoImage(file="/Users/yohaan_thetechgod/Desktop/Year 11 Stuff/Projects/Software Engineering/Software Assesment 1/Periodic table background.jpg")
-bg_image= tk.label(root,image= image_path)
-bg_image.place(relheight=1, relwidth=1)
 root = tk.Tk()
-root.title("Simple Quiz App")
+root.title("Elements quiz program")
 root.geometry("400x300")
 
 
