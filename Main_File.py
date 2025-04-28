aa95293decfc1555aa71d17608f9e1dea07d6e61
Yohaan-TheTechGod <yohaandosaj123@gmail.com>
2025-04-28 12:32:14 +1000

    fixed syntax error in main file

--- Main_File.py
+++ Main_File.py
@@ -55,7 +55,7 @@ def main_page():
         "text_color": 'white',
         "corner_radius": 20,
         "fg_color": "#818181",
-        "hover_color": "#black",
+        "hover_color": "black",
         "bg_color": "black",  
         "border_width": 5,
         "border_color": 'white'
